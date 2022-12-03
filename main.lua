ba5fd2a13b1df4153c8ad9fbdc91b22533bddcd6
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-03 11:25:17 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,21 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
 if game.PlaceId == 1335695570 then
 
-print("Supported!")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/DerpX/main/Games/Ninja-Legends/main.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6.5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
 
 else
 
-print("Not Supported!")
+Notification:Notify(
+    {Title = "Notification", Description = "Game Not Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6.5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
 
 end
