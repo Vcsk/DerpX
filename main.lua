d97f87ba74ec3236a54eb5e65b41e62b6ea03757
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-07 23:50:06 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,6 +19,15 @@ Notification:Notify(
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 
+elseif game.PlaceId == 11196588443 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/DerpX/main/Games/Zombie-Merge-Tycoon/main.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6.5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
 else
 
 Notification:Notify(
