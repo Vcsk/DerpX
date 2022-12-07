f9e50dc8b77dbf6dc97d4669117712a1bd973c30
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-07 21:19:38 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,6 +10,15 @@ Notification:Notify(
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
 
+elseif game.PlaceId == 7346416636 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/DerpX/main/Games/Pop-It-Trading/main.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6.5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
 else
 
 Notification:Notify(
