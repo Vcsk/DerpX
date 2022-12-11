58cf5c3e8865c403ce894d2a3a8dfabe2762ac85
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-11 11:37:24 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,34 +4,19 @@ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 if game.PlaceId == 3956818381 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/DerpX/main/Games/Ninja-Legends/main.lua", true))()
-Notification:Notify(
-    {Title = "Notification", Description = "Game Supported!"},
-    {OutlineColor = Color3.fromRGB(98, 37, 209),Time = 5, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
-)
 
 elseif game.PlaceId == 7346416636 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/DerpX/main/Games/Pop-It-Trading/main.lua", true))()
-Notification:Notify(
-    {Title = "Notification", Description = "Game Supported!"},
-    {OutlineColor = Color3.fromRGB(98, 37, 209),Time = 5, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
-)
 
 elseif game.PlaceId == 11196588443 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/DerpX/main/Games/Zombie-Merge-Tycoon/main.lua", true))()
-Notification:Notify(
-    {Title = "Notification", Description = "Game Supported!"},
-    {OutlineColor = Color3.fromRGB(98, 37, 209),Time = 5, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
-)
 
 else
 
 Notification:Notify(
-    {Title = "Notification", Description = "Game Not Supported!"},
+    {Title = "Error", Description = "Game Not Supported!"},
     {OutlineColor = Color3.fromRGB(98, 37, 209),Time = 5.5, Type = "image"},
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
 )
